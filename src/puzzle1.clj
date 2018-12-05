d7bcaafabfc852f811d7d6cd7dbec741fdd46b4f
Arjun Attam <arjunattam@gmail.com>
2018-12-05 16:09:29 +0530

    minor clean

--- src/puzzle1.clj
+++ src/puzzle1.clj
@@ -3,17 +3,16 @@
 (ns puzzle1
   (:require [clojure.string :as str]))
 
-(def test-input '(1 2 3))
+(defn solve [input]
+  (reduce + input))
+
+; tests
+(assert (= 6 (solve '(1 2 3))))
 
 (def parsed-input
   (for
       [x (str/split (slurp "inputs/puzzle1") #"\n")]
       (read-string x)))
-
-(defn solve [input]
-  (reduce + input))
-
-(assert (= 6 (solve test-input)))
 
 (defn -main []
   (println (solve parsed-input)))

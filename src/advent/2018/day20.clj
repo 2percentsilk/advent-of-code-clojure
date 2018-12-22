7349f73d29aea665b0c85307bde47b18b2c36c7f
Arjun Attam <arjunattam@gmail.com>
2018-12-22 15:57:57 +0530

    2018: day 20

--- src/advent/2018/day20.clj
+++ src/advent/2018/day20.clj
@@ -3,6 +3,12 @@
 
 (def test-input "^ENWWW(NEEE|SSE(EE|N))$")
 (def test-input-2 "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$")
+(def test-input-3 "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$")
+(def puzzle-input (slurp "inputs/2018/day20"))
+
+(def left-bracket \()
+(def right-bracket \))
+(def pipe \|)
 
 (defn letter? [c]
   (or (= c \W)
@@ -10,57 +16,67 @@
       (= c \N)
       (= c \S)))
 
-(defn get-options [pattern]
-  (loop [remaining pattern
-         options []
-         current-opt []
-         brack-stack '()]
-    (if-let [c (first remaining)]
-      (cond
-        (= c \() (recur (rest remaining)
-                        options
-                        (if (not (empty? brack-stack)) (conj current-opt c) current-opt)
-                        (conj brack-stack c))
-
-        (= c \)) (if (= brack-stack '(\())
-                   ; only one open bracket in the stack -> we are done with all options
-                   (->> current-opt
-                        (conj options)
-                        (mapv #(into % (rest remaining))))
-                   ; else
-                   (recur (rest remaining)
-                          options
-                          (conj current-opt c)
-                          (rest brack-stack)))
-
-        (= c \|) (if (= brack-stack '(\())
-                   (recur (rest remaining) (conj options current-opt) [] brack-stack)
-                   (recur (rest remaining) options (conj current-opt c) brack-stack))
-
-        :else (recur (rest remaining) options (conj current-opt c) brack-stack))
-      options)))
-
 (defn opposites? [c1 c2]
   (let [s (set (vector c1 c2))]
     (or (= s #{\N \S})
         (= s #{\E \W}))))
 
-(defn furthest-room [pattern moves-stack shortest-so-far]
-  (if-let [c (first pattern)]
-    (cond
-      (= c \^) (furthest-room (rest pattern) moves-stack shortest-so-far)
-      (= c \$) shortest-so-far
-      (letter? c) (furthest-room (rest pattern)
-                                 (if (opposites? (first moves-stack) c) (rest moves-stack)
-                                     (conj moves-stack c))
-                                 (if (opposites? (first moves-stack) c) (dec shortest-so-far)
-                                     (inc shortest-so-far)))
-      (= c \() (->> pattern
-                    get-options
-                    (mapv #(furthest-room % moves-stack shortest-so-far))
-                    (apply max))
-      :else shortest-so-far)))
+(defn max-count
+  ([x] x)
+  ([x y] (if (> (count x) (count y)) x y))
+  ([x y & more]
+   (reduce max-count (max-count x y) more)))
 
-(def puzzle-input (slurp "inputs/2018/day20"))
+(defn all-room-lengths [all-pattern]
+  (loop [pattern all-pattern
+         all-rooms []
+         moves-stack '()
+         start-for-level {}
+         opts-for-level {}
+         nesting-level 0]
+    (if-let [c (first pattern)]
+      (cond
+        (= c \^) (recur (rest pattern) all-rooms moves-stack start-for-level opts-for-level nesting-level)
+        (= c \$) all-rooms
 
-#_(furthest-room puzzle-input '() 0)
+        (and (letter? c)
+             (opposites? c (first moves-stack))) (recur (rest pattern) all-rooms
+                                                        (rest moves-stack)
+                                                        start-for-level opts-for-level nesting-level)
+
+        (and (letter? c)
+             (not (opposites? c (first moves-stack)))) (recur (rest pattern)
+                                                              (conj all-rooms (conj moves-stack c))
+                                                              (conj moves-stack c)
+                                                              start-for-level opts-for-level nesting-level)
+
+        (= c left-bracket) (recur (rest pattern) all-rooms moves-stack
+                                  (assoc start-for-level (inc nesting-level) moves-stack)
+                                  (assoc opts-for-level (inc nesting-level) [])
+                                  (inc nesting-level))
+
+        (= c pipe) (recur (rest pattern) all-rooms
+                          (get start-for-level nesting-level) start-for-level
+                          (update opts-for-level nesting-level #(conj % moves-stack))
+                          nesting-level)
+
+        (= c right-bracket) (let [all-opts (conj (get opts-for-level nesting-level) moves-stack)
+                                  best-moves-stack (apply max-count all-opts)]
+                              (recur (rest pattern) all-rooms best-moves-stack
+                                     (dissoc start-for-level nesting-level)
+                                     (dissoc opts-for-level nesting-level)
+                                     (dec nesting-level)))))))
+
+; Part 1
+#_(->> puzzle-input
+       all-room-lengths
+       (map #(count %))
+       (apply max))
+
+; Part 2
+#_(->> puzzle-input
+       all-room-lengths
+       (filter #(>= (count %) 1000))
+       (map #(frequencies %))
+       distinct
+       count)

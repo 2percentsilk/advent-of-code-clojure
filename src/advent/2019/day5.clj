26fdbfca344a90eb514dd12bfb40219d41022a33
Arjun Attam <Arjun.Attam@microsoft.com>
2019-12-08 00:34:32 +0000

    2019 day 7 part 1

--- src/advent/2019/day5.clj
+++ src/advent/2019/day5.clj
@@ -6,15 +6,16 @@
 (defn parse-input [input]
   (map read-string (str/split input #",")))
 
-(defn build-state [parsed]
+(defn build-state [parsed inputs]
   {:start 0
    :data (->> parsed
               (map-indexed hash-map)
               (into {}))
-   :outputs '()})
+   :outputs '()
+   :inputs inputs})
 
 (defn rev-digits [x] (if (= x 0) '()
-                     (conj (rev-digits (quot x 10)) (mod x 10))))
+                         (conj (rev-digits (quot x 10)) (mod x 10))))
 
 (defn parse-parameter [parameter]
   "returns indices of immediate mode parameters"
@@ -49,42 +50,46 @@
 
 (defn mul-action [state ins] (action state ins *))
 
-(defn run-once [{start :start data :data :as state} input]
+(defn jump-action [{start :start :as state} ins fn]
+  (assoc state
+         :start (if (fn 0 (para-i state ins 1))
+                  (para-i state ins 2)
+                  (+ 3 start))))
+
+(defn jump-if-true [state ins] (jump-action state ins not=))
+
+(defn jump-if-false [state ins] (jump-action state ins =))
+
+(defn compare-action [{start :start data :data :as state} ins fn]
+  (assoc state :start (+ 4 start)
+         :data (assoc data (get data (+ 3 start))
+                      (if (fn (para-i state ins 1) (para-i state ins 2)) 1 0))))
+
+(defn action-lt [state ins] (compare-action state ins <))
+
+(defn action-eq [state ins] (compare-action state ins =))
+
+(defn run-once [{start :start data :data inputs :inputs :as state}]
   (condp = (:op (parse-ins (get data start)))
     99 (assoc state :start nil)
     1 (add-action state (get data start))
     2 (mul-action state (get data start))
     3 (assoc state
              :start (+ 2 start)
-             :data (assoc data (get data (inc start)) input)) ; use 1 as input
+             :data (assoc data (get data (inc start)) (peek inputs))
+             :inputs (pop inputs))
     4 (assoc state
              :start (+ 2 start)
              :outputs (conj (:outputs state) (para-i state (get data start) 1)))
-    5 (assoc state 
-             :start (if (not= 0 (para-i state (get data start) 1))
-                      (para-i state (get data start) 2)
-                      (+ 3 start)))
-    6 (assoc state
-             :start (if (= 0 (para-i state (get data start) 1) )
-                      (para-i state (get data start) 2)
-                      (+ 3 start)))
-    7 (assoc state
-             :start (+ 4 start)
-             :data (assoc data (get data (+ 3 start))
-                          (if (< (para-i state (get data start) 1)
-                                 (para-i state (get data start) 2))
-                            1 0)))
-    8 (assoc state
-             :start (+ 4 start)
-             :data (assoc data (get data (+ 3 start))
-                          (if (= (para-i state (get data start) 1)
-                                 (para-i state (get data start) 2))
-                            1 0)))
+    5 (jump-if-true state (get data start))
+    6 (jump-if-false state (get data start))
+    7 (action-lt state (get data start))
+    8 (action-eq state (get data start))
     :else "error"))
 
-(defn run-all [{start :start :as state} input]
+(defn run-all [{start :start :as state}]
   (if (nil? start) state
-      (run-all (run-once state input) input)))
+      (run-all (run-once state))))
 
 ; (-> "1002,4,3,4,33"
 ;     parse-input
@@ -98,15 +103,17 @@
 
 ; Part 1
 ; :outputs (5074395 0 0 0 0 0 0 0 0 0)
-(def part-1
+#_(def part-1
   (-> puzzle-input
       parse-input
-      build-state
-      (run-all 1)))
+      (build-state '(1))
+      run-all
+      :outputs))
 
 ; Part 2: 8346937
-(def part-2
+#_(def part-2
   (-> puzzle-input
       parse-input
-      build-state
-      (run-all 5)))
+      (build-state '(5))
+      run-all
+      :outputs))
---
+++ src/advent/2019/day7.clj
@@ -0,0 +1,52 @@
+(ns advent.2019.day7
+  (:require [advent.2019.day5 :as base])
+  (:require [clojure.math.combinatorics :as combo]))
+
+(def puzzle-input (slurp "inputs/2019/day7"))
+
+(def test-program "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0")
+(def test-phase '(4 3 2 1 0))
+
+(defn run-amplifier [program phase input]
+  "runs one number, the output of the executions on an amplifier"
+  (-> program
+      base/parse-input
+      (base/build-state (conj '() input phase))
+      base/run-all
+      :outputs
+      first))
+
+(defn run-all-amplifiers [program phases current-input]
+  (if (empty? phases)
+    current-input
+    (run-all-amplifiers program (rest phases)
+                        (run-amplifier program (first phases) current-input))))
+
+(defn all-phases [phase-start n-amps]
+  (map seq (combo/permutations (range phase-start (+ phase-start n-amps)))))
+
+(defn highest-output [program start-input n-amps phase-start]
+  (loop [phases (all-phases phase-start n-amps)
+         best-so-far 0]
+    (if (empty? phases) best-so-far
+        (recur (rest phases)
+               (let [res (run-all-amplifiers program 
+                                             (first phases) 
+                                             start-input)]
+                 (if (> res best-so-far) res best-so-far))))))
+
+; Part 1: 844468
+#_(def part-1
+    (highest-output puzzle-input 0 5 0))
+
+(highest-output
+ "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+ 0 5 5)
+
+; (def test-input )
+
+; (def part-1
+;   (-> puzzle-input-day7
+;       parse-input
+;       build-state
+;       (run-all 1)))

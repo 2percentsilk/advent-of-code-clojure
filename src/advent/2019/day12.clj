22951706ace235f428b7dd08f33ac96f06e7328a
Arjun Attam <Arjun.Attam@microsoft.com>
2019-12-18 02:14:30 +0000

    2019 day 12 part 1

--- src/advent/2019/day12.clj
+++ src/advent/2019/day12.clj
@@ -1,18 +1,11 @@
 (ns advent.2019.day12
-  (:require [clojure.string :as str])
-  (:require [clojure.math.numeric-tower :as math]))
-
-(def test-pos
-  '("<x=-1, y=0, z=2>"
-    "<x=2, y=-10, z=-7>"
-    "<x=4, y=-8, z=8>"
-    "<x=3, y=5, z=-1>"))
+  (:require [clojure.string :as str]))
 
 (def x
-  [{:x -1 :y 0 :z 2}
-   {:x 2 :y -10 :z -7}
-   {:x 4 :y -8 :z 8}
-   {:x 3 :y 5 :z -1}])
+  [{:x 17 :y -9 :z 4}
+   {:x 2 :y 2 :z -13}
+   {:x -1 :y 5 :z -1}
+   {:x 4 :y 7 :z -7}])
 
 (def v
   [{:x 0 :y 0 :z 0}
@@ -22,12 +15,34 @@
 
 (def state (map (fn [x y] {:p x :v y}) x v))
 
-(defn v-offset [{p1 :p v1 :v} state]
-  (map #({:x (v-dim-offset (:x p1) ())}) state))
+(defn v-dim-offset [{p0 :p} state dim]
+  (->> state
+       (map (fn [{p :p}] (cond
+                           (> (dim p) (dim p0)) 1
+                           (< (dim p) (dim p0)) -1
+                           :else 0)))
+       (reduce +)))
 
-(apply v-offset (first (partition 2 1 state)))
+(defn v-offset [state]
+  (map (fn [x] {:x (v-dim-offset x state :x)
+                :y (v-dim-offset x state :y)
+                :z (v-dim-offset x state :z)}) state))
 
-(defn apply-gravity [x v]
-  )
+(defn apply-gravity [state]
+  (map (fn [{v :v :as moon} y] (assoc moon :v (merge-with + v y)))
+       state (v-offset state)))
 
-(defn time-step [x v])
+(defn apply-velocity [state]
+  (map (fn [{v :v p :p :as moon}] (assoc moon :p (merge-with + p v))) state))
+
+(defn time-step [state] (apply-velocity (apply-gravity state)))
+
+(defn abs [x] (if (< x 0) (- x) x))
+
+(defn energy-moon [{p :p v :v}]
+  (* (reduce + (map abs (vals p)))
+     (reduce + (map abs (vals v)))))
+
+(defn energy [state] (reduce + (map energy-moon state)))
+
+(defn -main [] (println (energy (nth (iterate time-step state) 1000))))
